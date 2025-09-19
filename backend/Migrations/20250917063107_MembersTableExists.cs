1e0ba76e41a4273877bd9daf559bd1ed277d55dc
peter <163820447+pp20240318@users.noreply.github.com>
2025-09-19 15:17:35 +0800

    1

---
+++ backend/Migrations/20250917063107_MembersTableExists.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCms.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MembersTableExists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Members table already exists, so this is an empty migration
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
